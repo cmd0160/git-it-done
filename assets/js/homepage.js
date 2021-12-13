593639f4643bac35b81d758412f6d9e2b7a5e98c
CD MacBook <corydavis1989@gmail.com>
2021-12-13 14:04:43 -0600

    initial commit

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -10,4 +10,19 @@ fetch(apiUrl).then(function(response){
 });
 };
 
-getUserRepos();
+var userFormEl = document.querySelector("#user-form");
+var nameInputEl = document.querySelector("#username");
+
+var formSubmitHandler = function(event) {
+    event.preventDefault();
+    var username = nameInputEl.value.trim();
+
+    if(username) {
+    getUserRepos(username);
+    nameInputEl.value = "";
+    } else {
+        alert("Please enter a GitHub username");
+    }
+};
+
+userFormEl.addEventListener("submit", formSubmitHandler);
